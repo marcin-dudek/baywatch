97de56ece94956610fcd1fc66185de5c78c37300
Marcin Dudek <manekd@gmail.com>
2018-09-16 23:03:49 +0200

    Move code to src directory

---
+++ src/configuration.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// Config contains configuration information
+type Config struct {
+	Request  []Request `json:"request"`
+	Interval Duration  `json:"interval"`
+}
+
+// Request represents single
+type Request struct {
+	URL    string            `json:"url"`
+	Method string            `json:"method"`
+	Header map[string]string `json:"header"`
+	Body   string            `json:"body"`
+}
+
+func readConfiguration() Config {
+	configFile, err := os.Open("config.json")
+	if err != nil {
+		log.WithFields(log.Fields{"Error": err.Error()}).Error("opening config file")
+	}
+
+	var config Config
+
+	jsonParser := json.NewDecoder(configFile)
+	if err = jsonParser.Decode(&config); err != nil {
+		log.WithFields(log.Fields{"Error": err.Error()}).Error("parsing config file")
+	}
+
+	return config
+}
+
+// Duration copy
+type Duration struct {
+	time.Duration
+}
+
+// MarshalJSON convert duration to string
+func (d Duration) MarshalJSON() ([]byte, error) {
+	return json.Marshal(d.String())
+}
+
+// UnmarshalJSON converts back
+func (d *Duration) UnmarshalJSON(b []byte) error {
+	var v interface{}
+	if err := json.Unmarshal(b, &v); err != nil {
+		return err
+	}
+	switch value := v.(type) {
+	case float64:
+		d.Duration = time.Duration(value)
+		return nil
+	case string:
+		var err error
+		d.Duration, err = time.ParseDuration(value)
+		if err != nil {
+			return err
+		}
+		return nil
+	default:
+		return errors.New("invalid duration")
+	}
+}
